2333e5f5645475ba61f85213bb19aaf3b85fc1bb
fenner <fenner>
1999-12-15 06:49:05 +0000

    Get rid of /* inside comment.

--- nfsfh.h
+++ nfsfh.h
@@ -1,12 +1,12 @@
 /*
- * $Header: /tcpdump/master/tcpdump/nfsfh.h,v 1.7 1999-11-21 05:43:49 assar Exp $
+ * $Header: /tcpdump/master/tcpdump/nfsfh.h,v 1.8 1999-12-15 06:49:05 fenner Exp $
  *
  * nfsfh.h - NFS file handle definitions (for portable use)
  *
  * Jeffrey C. Mogul
  * Digital Equipment Corporation
  * Western Research Laboratory
-/*	$NetBSD: nfsfh.h,v 1.1.1.2 1997/10/03 17:25:13 christos Exp $	*/
+ *	$NetBSD: nfsfh.h,v 1.1.1.2 1997/10/03 17:25:13 christos Exp $	*/
 
 /*
  * Internal representation of dev_t, because different NFS servers
