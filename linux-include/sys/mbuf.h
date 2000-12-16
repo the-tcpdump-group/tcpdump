4722d062ac3784419b29e549d128c21374eb06fc
guy <guy>
2000-12-16 21:52:23 +0000

    The stuff in the "linux-include" directory is no longer needed, as we no longer depend on the OS's header files for packet format and packet field value definitions; get rid of the stuff in that directory.

--- linux-include/sys/mbuf.h
+++
@@ -1,5 +0,0 @@
-/* @(#) $Header: /tcpdump/master/tcpdump/linux-include/sys/Attic/mbuf.h,v 1.1.1.1 1999-10-07 23:47:15 mcr Exp $ (LBL) */
-
-#ifndef MLEN
-#define MLEN 128			/* needed for slcompress.h */
-#endif
