fb5749bcb62f0bc4659e83d48366324d516dec80
itojun <itojun>
2001-06-15 07:45:42 +0000

    style

--- print-bxxp.c
+++ print-bxxp.c
@@ -11,7 +11,7 @@
 
 #ifndef lint
 static const char rcsid[] =
-  "@(#) $Header: /tcpdump/master/tcpdump/Attic/print-bxxp.c,v 1.3 2000-10-05 04:10:01 itojun Exp $";
+  "@(#) $Header: /tcpdump/master/tcpdump/Attic/print-bxxp.c,v 1.4 2001-06-15 07:45:42 itojun Exp $";
 #endif
 
 #ifdef HAVE_CONFIG_H
@@ -38,45 +38,30 @@ static const char rcsid[] =
  * Looks at the first few chars up to tl1 ...
  */
 
-int l_strnstart(const u_char *, u_int, const u_char *, u_int);
+static int l_strnstart(const u_char *, u_int, const u_char *, u_int);
 
-int
-l_strnstart(register const u_char *tstr1, register u_int tl1,
-	  register const u_char *str2, register u_int l2)
+static int
+l_strnstart(const u_char *tstr1, u_int tl1, const u_char *str2, u_int l2)
 {
 
-  if (tl1 > l2)
-    return 0;
-  
-  return (strncmp(tstr1, str2, tl1) == 0 ? 1 : 0);
+	if (tl1 > l2)
+		return 0;
 
+	return (strncmp(tstr1, str2, tl1) == 0 ? 1 : 0);
 }
 
 void
-bxxp_print(register const u_char *bp, register u_int length)
+bxxp_print(const u_char *bp, u_int length)
 {
 
-  if (l_strnstart("REQ ", 4, bp, length)) { /* A REQuest */
-
-    printf(" BXXP REQ");
-
-  } 
-  else if (l_strnstart("RSP ", 4, bp, length)) {
-
-    printf(" BXXP RSP");
-
-  }
-  else if (l_strnstart("SEQ ", 4, bp, length)) {
-
-    printf(" BXXP SEQ");
-
-  }
-  else if (l_strnstart("END", 4, bp, length)) {
-
-    printf(" BXXP END");
-
-  }
-  else 
-    printf(" BXXP (payload or undecoded)");
-
+	if (l_strnstart("REQ ", 4, bp, length)) /* A REQuest */
+		printf(" BXXP REQ");
+	else if (l_strnstart("RSP ", 4, bp, length))
+		printf(" BXXP RSP");
+	else if (l_strnstart("SEQ ", 4, bp, length))
+		printf(" BXXP SEQ");
+	else if (l_strnstart("END", 4, bp, length))
+		printf(" BXXP END");
+	else 
+		printf(" BXXP (payload or undecoded)");
 }
