3f25a3f3d315b220a53f4e6283762692e4698d48
itojun <itojun>
2001-10-22 06:58:33 +0000

    copyright clarification.

--- print-radius.c
+++ print-radius.c
@@ -1,4 +1,25 @@
 /*
+ * Copyright (C) 2000 Alfredo Andres Omella.  All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *   3. The names of the authors may not be used to endorse or promote
+ *      products derived from this software without specific prior
+ *      written permission.
+ *  
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+/*
  * Radius printer routines as specified on:
  *
  * RFC 2865:
@@ -23,7 +44,7 @@
 
 #ifndef lint
 static const char rcsid[] =
-    "$Id: print-radius.c,v 1.9 2001-07-15 19:27:45 guy Exp $";
+    "$Id: print-radius.c,v 1.10 2001-10-22 06:58:33 itojun Exp $";
 #endif
 
 #ifdef HAVE_CONFIG_H
