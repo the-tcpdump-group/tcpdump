2ff5bd4a8de01b28d77a460fefcb547f7511b064
itojun <itojun>
2001-10-22 04:23:37 +0000

    copyright clarification.

--- rx.h
+++ rx.h
@@ -1,7 +1,29 @@
 /*
+ * Copyright: (c) 2000 United States Government as represented by the
+ *	Secretary of the Navy. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *   3. The names of the authors may not be used to endorse or promote
+ *      products derived from this software without specific prior
+ *      written permission.
+ *  
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+/*
  * Rx protocol format
  *
- * $Id: rx.h,v 1.4 2001-09-09 01:41:50 guy Exp $
+ * $Id: rx.h,v 1.5 2001-10-22 04:23:37 itojun Exp $
  */
 
 #define FS_RX_PORT	7000
