24ec8362934ef96ea153ba8f208e41b98e5039a5
itojun <itojun>
2002-06-11 17:15:30 +0000

    __ss_len/family is now nonstandard.  standards were clarified to use ss_len/family.

--- missing/sockstorage.h
+++ missing/sockstorage.h
@@ -28,11 +28,11 @@
  */
 struct sockaddr_storage {
 #ifdef HAVE_SOCKADDR_SA_LEN
-	u_int8_t __ss_len;
-	u_int8_t __ss_family;
+	u_int8_t ss_len;
+	u_int8_t ss_family;
 	u_int8_t fill[126];
 #else
-	u_int8_t __ss_family;
+	u_int8_t ss_family;
 	u_int8_t fill[127];
 #endif /* HAVE_SOCKADDR_SA_LEN */
 };
