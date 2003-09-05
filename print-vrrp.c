cc77dd3860c260db0a18a4f049fd487da2877cc2
hannes <hannes>
2003-09-05 17:42:41 +0000

    minor display cosmetics to get to an consistent output style

--- print-vrrp.c
+++ print-vrrp.c
@@ -25,7 +25,7 @@
 
 #ifndef lint
 static const char rcsid[] =
-    "@(#) $Header: /tcpdump/master/tcpdump/print-vrrp.c,v 1.6 2002-08-01 08:53:34 risso Exp $";
+    "@(#) $Header: /tcpdump/master/tcpdump/print-vrrp.c,v 1.7 2003-09-05 17:42:41 hannes Exp $";
 #endif
 
 #ifdef HAVE_CONFIG_H
@@ -68,7 +68,7 @@ static const char rcsid[] =
 #define	VRRP_TYPE_ADVERTISEMENT	1
 
 static const struct tok type2str[] = {
-	{ VRRP_TYPE_ADVERTISEMENT,	"advertisement"	},
+	{ VRRP_TYPE_ADVERTISEMENT,	"Advertisement"	},
 	{ 0,				NULL		}
 };
 
@@ -93,27 +93,27 @@ vrrp_print(register const u_char *bp, register u_int len, int ttl)
 	TCHECK(bp[0]);
 	version = (bp[0] & 0xf0) >> 4;
 	type = bp[0] & 0x0f;
-	type_s = tok2str(type2str, "type#%d", type);
-	printf("VRRPv%d-%s %d: ", version, type_s, len);
+	type_s = tok2str(type2str, "unknown type (%u)", type);
+	printf("VRRPv%u, %s", version, type_s);
 	if (ttl != 255)
-		printf("[ttl=%d!] ", ttl);
+		printf(", (ttl %u)", ttl);
 	if (version != 2 || type != VRRP_TYPE_ADVERTISEMENT)
 		return;
 	TCHECK(bp[2]);
-	printf("vrid=%d prio=%d", bp[1], bp[2]);
+	printf(", vrid %u, prio %u", bp[1], bp[2]);
 	TCHECK(bp[5]);
 	auth_type = bp[4];
-	printf(" authtype=%s", tok2str(auth2str, NULL, auth_type));
-	printf(" intvl=%d", bp[5]);
+	printf(", authtype %s", tok2str(auth2str, NULL, auth_type));
+	printf(", intvl %us, length %u", bp[5],len);
 	if (vflag) {
 		int naddrs = bp[3];
 		int i;
 		char c;
 
 		if (TTEST2(bp[0], len) && in_cksum((const u_short*)bp, len, 0))
-			printf(" (bad vrrp cksum %x!)",
+			printf(", (bad vrrp cksum %x)",
 				EXTRACT_16BITS(&bp[6]));
-		printf(" addrs");
+		printf(", addrs");
 		if (naddrs > 1)
 			printf("(%d)", naddrs);
 		printf(":");
