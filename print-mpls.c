4011b8d35d4c5e86fdb9998338126ffbf28745a7
hannes <hannes>
2004-06-14 14:47:58 +0000

    make MPLS label handling macros available to the outside world

--- print-mpls.c
+++ print-mpls.c
@@ -28,7 +28,7 @@
 
 #ifndef lint
 static const char rcsid[] _U_ =
-    "@(#) $Header: /tcpdump/master/tcpdump/print-mpls.c,v 1.11 2004-06-06 19:20:04 hannes Exp $ (LBL)";
+    "@(#) $Header: /tcpdump/master/tcpdump/print-mpls.c,v 1.12 2004-06-14 14:47:58 hannes Exp $ (LBL)";
 #endif
 
 #ifdef HAVE_CONFIG_H
@@ -44,20 +44,7 @@ static const char rcsid[] _U_ =
 #include "addrtoname.h"
 #include "interface.h"
 #include "extract.h"			/* must come after interface.h */
-
-#define LABEL_MASK	0xfffff000
-#define LABEL_SHIFT	12
-#define	EXP_MASK	0x00000e00
-#define EXP_SHIFT	9
-#define	STACK_MASK	0x00000100
-#define STACK_SHIFT	8
-#define TTL_MASK	0x000000ff
-#define TTL_SHIFT	0
-
-#define MPLS_LABEL(x)	(((x) & LABEL_MASK) >> LABEL_SHIFT)
-#define MPLS_EXP(x)	(((x) & EXP_MASK) >> EXP_SHIFT)
-#define MPLS_STACK(x)	(((x) & STACK_MASK) >> STACK_SHIFT)
-#define MPLS_TTL(x)	(((x) & TTL_MASK) >> TTL_SHIFT)
+#include "mpls.h"
 
 static const char *mpls_labelname[] = {
 /*0*/	"IPv4 explicit NULL", "router alert", "IPv6 explicit NULL",
