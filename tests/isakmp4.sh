289b2a35d4408f8d25a8e369a7c1083d01392861
mcr <mcr>
2005-04-07 00:20:51 +0000

    added ESP-in-UDP test cases.

--- tests/isakmp4.sh
+++ tests/isakmp4.sh
@@ -3,8 +3,8 @@
 uudecode isakmp4500.puu
 
 echo -n test isakmp4...
-../tcpdump -t -n -E "file esp-secrets.txt" -r isakmp4500.pcap >isakmp4500.new
-if diff isakmp4500.new isakmp4500.out
+../tcpdump -t -n -E "file esp-secrets.txt" -r isakmp4500.pcap >isakmp4.new
+if diff isakmp4.new isakmp4.out
 then
 	echo passed.
 else
