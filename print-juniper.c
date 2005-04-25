6b244042659597c052663c6d2d7ed83db421ec25
guy <guy>
2005-04-25 18:55:01 +0000

    From Albert Chin: IBM's C compiler, at least for AIX 4.3.3, doesn't allow a trailing comma on the last member of an enum.

--- print-juniper.c
+++ print-juniper.c
@@ -15,7 +15,7 @@
 
 #ifndef lint
 static const char rcsid[] _U_ =
-    "@(#) $Header: /tcpdump/master/tcpdump/print-juniper.c,v 1.8.2.2 2005-04-21 04:04:01 guy Exp $ (LBL)";
+    "@(#) $Header: /tcpdump/master/tcpdump/print-juniper.c,v 1.8.2.3 2005-04-25 18:55:01 guy Exp $ (LBL)";
 #endif
 
 #ifdef HAVE_CONFIG_H
@@ -56,7 +56,7 @@ enum {
 
 enum {
     DEFAULT,
-    LS_COOKIE,
+    LS_COOKIE
 };
 
 struct juniper_cookie_table_t {
