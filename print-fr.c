0a0363f416bd63befe18bb69737e61c4e5394af6
guy <guy>
2005-04-25 19:17:25 +0000

    Dereference the *right* function pointer.

--- print-fr.c
+++ print-fr.c
@@ -21,7 +21,7 @@
 
 #ifndef lint
 static const char rcsid[] _U_ =
-	"@(#)$Header: /tcpdump/master/tcpdump/print-fr.c,v 1.33 2005-04-25 16:29:00 guy Exp $ (LBL)";
+	"@(#)$Header: /tcpdump/master/tcpdump/print-fr.c,v 1.34 2005-04-25 19:17:25 guy Exp $ (LBL)";
 #endif
 
 #ifdef HAVE_CONFIG_H
@@ -575,7 +575,7 @@ q933_print(const u_char *p, u_int length)
                            ie_p->ie_len);
                     
                 if (!fr_q933_print_ie_codeset[codeset] ||
-                    (*fr_q933_print_ie_codeset)(ie_p, ptemp)) {
+                    (*fr_q933_print_ie_codeset[codeset])(ie_p, ptemp)) {
                     if (vflag <= 1)
                         print_unknown_data(ptemp+2,"\n\t",ie_p->ie_len);
                 }
