b87c2e45492a77b258d8f47a17b8614c01c5f707
hannes <hannes>
2005-05-12 07:23:04 +0000

    add support for DLT_JUNIPER_PPPOE_ATM printer replace the JUNIPER_ enums with real DLT_JUNIPER types   (does this break environments where an old libpcap is present ?)

--- interface.h
+++ interface.h
@@ -18,7 +18,7 @@
  * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * @(#) $Header: /tcpdump/master/tcpdump/interface.h,v 1.244.2.6 2005-05-06 07:57:17 guy Exp $ (LBL)
+ * @(#) $Header: /tcpdump/master/tcpdump/interface.h,v 1.244.2.7 2005-05-12 07:23:05 hannes Exp $ (LBL)
  */
 
 #ifndef tcpdump_interface_h
@@ -255,6 +255,7 @@ extern u_int juniper_mfr_print(const struct pcap_pkthdr *, register const u_char
 extern u_int juniper_mlfr_print(const struct pcap_pkthdr *, const u_char *);
 extern u_int juniper_mlppp_print(const struct pcap_pkthdr *, const u_char *);
 extern u_int juniper_pppoe_print(const struct pcap_pkthdr *, const u_char *);
+extern u_int juniper_pppoe_atm_print(const struct pcap_pkthdr *, const u_char *);
 extern u_int sll_if_print(const struct pcap_pkthdr *, const u_char *);
 extern void snmp_print(const u_char *, u_int);
 extern void sunrpcrequest_print(const u_char *, u_int, const u_char *);
--- tcpdump.c
+++ tcpdump.c
@@ -30,7 +30,7 @@ static const char copyright[] _U_ =
     "@(#) Copyright (c) 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 2000\n\
 The Regents of the University of California.  All rights reserved.\n";
 static const char rcsid[] _U_ =
-    "@(#) $Header: /tcpdump/master/tcpdump/tcpdump.c,v 1.253.2.2 2005-05-03 20:39:25 hannes Exp $ (LBL)";
+    "@(#) $Header: /tcpdump/master/tcpdump/tcpdump.c,v 1.253.2.3 2005-05-12 07:23:04 hannes Exp $ (LBL)";
 #endif
 
 /*
@@ -237,6 +237,12 @@ static struct printer printers[] = {
 #endif
 #ifdef DLT_JUNIPER_PPPOE
 	{ juniper_pppoe_print,	DLT_JUNIPER_PPPOE },
+#endif
+#ifdef DLT_JUNIPER_PPPOE
+	{ juniper_pppoe_print,	DLT_JUNIPER_PPPOE },
+#endif
+#ifdef DLT_JUNIPER_PPPOE_ATM
+	{ juniper_pppoe_atm_print, DLT_JUNIPER_PPPOE_ATM },
 #endif
 	{ NULL,			0 },
 };
