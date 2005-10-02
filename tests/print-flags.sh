6405d334cf6a878b74d3d71446df38b96c816fe2
mcr <mcr>
2005-10-02 22:24:31 +0000

    updated test case output

--- tests/print-flags.sh
+++ tests/print-flags.sh
@@ -3,7 +3,7 @@
 uudecode print-flags.puu
 
 for i in x xx X XX A AA; do
-	if (../tcpdump -$i -s0 -nr print-flags.pcap | diff - print-$i.out)
+	if (../tcpdump -$i -s0 -nr print-flags.pcap | tee print-$i.new | diff - print-$i.out)
 	then
 		echo print-$i passed.
 	else
