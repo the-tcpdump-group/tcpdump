4cc8fb645489fbc1afd74bc01ee937e9963731d2
guy <guy>
2007-07-14 22:29:05 +0000

    Fix message printed for bad andX offset to correctly describe the badness.

--- print-smb.c
+++ print-smb.c
@@ -12,7 +12,7 @@
 
 #ifndef lint
 static const char rcsid[] _U_ =
-     "@(#) $Header: /tcpdump/master/tcpdump/print-smb.c,v 1.41.2.3 2007-07-14 21:10:19 guy Exp $";
+     "@(#) $Header: /tcpdump/master/tcpdump/print-smb.c,v 1.41.2.4 2007-07-14 22:29:05 guy Exp $";
 #endif
 
 #include <tcpdump-stdinc.h>
@@ -905,7 +905,7 @@ print_smb(const u_char *buf, const u_char *maxbuf)
 	printf("\nSMB PACKET: %s (%s) (CHAINED)\n",
 	    fn->name, request ? "REQUEST" : "REPLY");
 	if (newsmboffset <= smboffset) {
-	    printf("Bad andX offset: %u < %u\n", newsmboffset, smboffset);
+	    printf("Bad andX offset: %u <= %u\n", newsmboffset, smboffset);
 	    break;
 	}
 	smboffset = newsmboffset;
