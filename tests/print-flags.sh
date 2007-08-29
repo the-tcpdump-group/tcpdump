af83d22daeb4189a8a556005ecd419cc3576d3f7
mcr <mcr>
2007-08-29 12:04:17 +0000

    test cases and output for IKEv2 and -X options.

--- tests/print-flags.sh
+++ tests/print-flags.sh
@@ -3,7 +3,7 @@
 uudecode print-flags.puu
 
 for i in x xx X XX A AA; do
-	if (../tcpdump -$i -s0 -nr print-flags.pcap | tee print-$i.new | diff - print-$i.out)
+	if (../tcpdump -$i -s0 -nr print-flags.pcap | tee NEW/print-$i.new | diff - print-$i.out >DIFF/print-$i.out.diff )
 	then
 		echo print-$i passed.
 	else
