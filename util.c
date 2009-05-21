c19f2e8f2b2bfd57a9582552bf0eab769a0e19b1
Guy Harris <gharris@steve.local>
2009-05-21 10:32:59 -0700

    Squelch a warning, style normalization.

--- util.c
+++ util.c
@@ -415,7 +415,7 @@ tok2strary_internal(register const char **lp, int n, register const char *fmt,
  */
 
 int
-mask2plen (u_int32_t mask)
+mask2plen(u_int32_t mask)
 {
 	u_int32_t bitmasks[33] = {
 		0x00000000,
@@ -440,26 +440,26 @@ mask2plen (u_int32_t mask)
 }
 
 #ifdef INET6
-int mask62plen(const u_char *mask)
+int
+mask62plen(const u_char *mask)
 {
-	u_char bitmasks[] =
-	{
-		0x00, 0x80, 0xc0, 0xe0,
-		0xf0, 0xf8, 0xfc, 0xfe, 0xff
+	u_char bitmasks[9] = {
+		0x00,
+		0x80, 0xc0, 0xe0, 0xf0,
+		0xf8, 0xfc, 0xfe, 0xff
 	};
-	int cidr_len = 0;
 	int byte;
+	int cidr_len = 0;
 
-	for (byte = 0; byte < 16; byte++)
-	{
-		int bits;
+	for (byte = 0; byte < 16; byte++) {
+		u_int bits;
 
-		for (bits = 0; bits < (sizeof (bitmasks) / sizeof (bitmasks[0])); bits++)
-			if (mask[byte] == bitmasks[bits])
-			{
+		for (bits = 0; bits < (sizeof (bitmasks) / sizeof (bitmasks[0])); bits++) {
+			if (mask[byte] == bitmasks[bits]) {
 				cidr_len += bits;
 				break;
 			}
+		}
 
 		if (mask[byte] != 0xff)
 			break;
