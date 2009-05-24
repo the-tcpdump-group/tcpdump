4ba67d9a002e30b5873528d9b66711999902878d
Guy Harris <gharris@steve.local>
2009-05-24 13:25:35 -0700

    Get rid of __attribute__((__packed__)) - not all compilers support it, so we can't rely on it, and it shouldn't be necessary in any case (all items in that structure are aligned on their natural boundaries, and the structure length is a multiple of the natural boundary of the most strictly aligned member).

--- ieee802_11_radio.h
+++ ieee802_11_radio.h
@@ -73,7 +73,7 @@ struct ieee80211_radiotap_header {
 					 * Additional extensions are made
 					 * by setting bit 31.
 					 */
-} __attribute__((__packed__));
+};
 
 /* Name                                 Data type       Units
  * ----                                 ---------       -----
