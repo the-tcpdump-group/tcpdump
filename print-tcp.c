29d83dbb61be640899d969357f2a8e1c3e02c7ee
Denis Ovsienko <infrastation@yandex.ru>
2013-11-07 11:07:13 +0400

    print-tcp: add some NFS printing finish
    Add printing of direction and restore printing of the transaction ID,
    which print_nfsaddr() used to output in a way.

--- print-tcp.c
+++ print-tcp.c
@@ -722,12 +722,12 @@ tcp_print(register const u_char *bp, register u_int length,
                 if (TTEST(rp->rm_direction)) {
                         direction = (enum sunrpc_msg_type)EXTRACT_32BITS(&rp->rm_direction);
                         if (dport == NFS_PORT && direction == SUNRPC_CALL) {
-                                (void)printf(" | ");
+                                (void)printf(": NFS request xid %u ", EXTRACT_32BITS(&rp->rm_xid));
                                 nfsreq_print_noaddr((u_char *)rp, fraglen, (u_char *)ip);
                                 return;
                         }
                         if (sport == NFS_PORT && direction == SUNRPC_REPLY) {
-                                (void)printf(" | ");
+                                (void)printf(": NFS reply xid %u ", EXTRACT_32BITS(&rp->rm_xid));
                                 nfsreply_print_noaddr((u_char *)rp, fraglen, (u_char *)ip);
                                 return;
                         }
