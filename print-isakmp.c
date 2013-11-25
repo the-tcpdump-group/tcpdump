3228c64ab1a6f4c33dda42c094aa69918bfffbbd
Gleb Smirnoff <glebius@FreeBSD.org>
2013-11-26 00:16:27 +0400

    Fix compilation warnings: - set but unused variables - type punned casts

--- print-isakmp.c
+++ print-isakmp.c
@@ -547,7 +547,6 @@ ikev1_sa_print(netdissect_options *ndo, u_char tpay _U_,
 {
 	const struct ikev1_pl_sa *p;
 	struct ikev1_pl_sa sa;
-	const u_int32_t *q;
 	u_int32_t doi, sit, ident;
 	const u_char *cp, *np;
 	int t;
@@ -566,7 +565,6 @@ ikev1_sa_print(netdissect_options *ndo, u_char tpay _U_,
 	}
 
 	ND_PRINT((ndo," doi=ipsec"));
-	q = (u_int32_t *)&sa.sit;
 	ND_PRINT((ndo," situation="));
 	t = 0;
 	if (sit & 0x01) {
@@ -1770,7 +1768,6 @@ ikev2_n_print(netdissect_options *ndo, u_char tpay _U_,
 {
 	struct ikev2_n *p, n;
 	const u_char *cp;
-	u_char *ep2;
 	u_char showspi, showdata, showsomedata;
 	const char *notify_name;
 	u_int32_t type;
@@ -1947,7 +1944,6 @@ ikev2_n_print(netdissect_options *ndo, u_char tpay _U_,
 	}
 
 	cp = (u_char *)(p + 1) + n.spi_size;
-	ep2 = (u_char *)p + item_len;
 
 	if(3 < ndo->ndo_vflag) {
 		showdata = 1;
@@ -2219,7 +2215,7 @@ ikev1_print(netdissect_options *ndo,
 	p = (const struct isakmp *)bp;
 	ep = ndo->ndo_snapend;
 	
-	phase = (*(u_int32_t *)base->msgid == 0) ? 1 : 2;
+	phase = (EXTRACT_32BITS(base->msgid) == 0) ? 1 : 2;
 	if (phase == 1)
 		ND_PRINT((ndo," phase %d", phase));
 	else
@@ -2251,7 +2247,6 @@ ikev1_print(netdissect_options *ndo,
 	
 	if (ndo->ndo_vflag) {
 		const struct isakmp_gen *ext;
-		int nparen;
 		
 		ND_PRINT((ndo,":"));
 		
@@ -2265,7 +2260,6 @@ ikev1_print(netdissect_options *ndo,
 			goto done;
 		}
 		
-		nparen = 0;
 		CHECKLEN(p + 1, base->np);
 		np = base->np;
 		ext = (struct isakmp_gen *)(p + 1);
@@ -2390,7 +2384,7 @@ ikev2_print(netdissect_options *ndo,
 	p = (const struct isakmp *)bp;
 	ep = ndo->ndo_snapend;
 
-	phase = (*(u_int32_t *)base->msgid == 0) ? 1 : 2;
+	phase = (EXTRACT_32BITS(base->msgid) == 0) ? 1 : 2;
 	if (phase == 1)
 		ND_PRINT((ndo, " parent_sa"));
 	else
@@ -2406,7 +2400,6 @@ ikev2_print(netdissect_options *ndo,
 
 	if (ndo->ndo_vflag) {
 		const struct isakmp_gen *ext;
-		int nparen;
 
 		ND_PRINT((ndo, ":"));
 
@@ -2420,7 +2413,6 @@ ikev2_print(netdissect_options *ndo,
 			goto done;
 		}
 
-		nparen = 0;
 		CHECKLEN(p + 1, base->np)
 
 		np = base->np;
@@ -2504,8 +2496,6 @@ isakmp_rfc3948_print(netdissect_options *ndo,
 		     const u_char *bp, u_int length,
 		     const u_char *bp2)
 {
-	const u_char *ep;
-	ep = ndo->ndo_snapend;
 
 	if(length == 1 && bp[0]==0xff) {
 		ND_PRINT((ndo, "isakmp-nat-keep-alive"));
