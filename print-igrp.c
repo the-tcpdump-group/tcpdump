5af9c5ca76979f374d6bab8c01550cd5b716263d
Guy Harris <guy@alum.mit.edu>
2013-12-31 02:35:24 -0800

    Move stuff from igrp.h to print-igrp.c.

--- print-igrp.c
+++ print-igrp.c
@@ -36,9 +36,41 @@ static const char rcsid[] _U_ =
 
 #include "interface.h"
 #include "addrtoname.h"
-#include "igrp.h"
 #include "ip.h"
 #include "extract.h"			/* must come after interface.h */
+
+/* Cisco IGRP definitions */
+
+/* IGRP Header */
+
+struct igrphdr {
+	u_int8_t ig_vop;	/* protocol version number / opcode */
+#define IGRP_V(x)	(((x) & 0xf0) >> 4)
+#define IGRP_OP(x)	((x) & 0x0f)
+	u_int8_t ig_ed;		/* edition number */
+	u_int16_t ig_as;	/* autonomous system number */
+	u_int16_t ig_ni;	/* number of subnet in local net */
+	u_int16_t ig_ns;	/* number of networks in AS */
+	u_int16_t ig_nx;	/* number of networks ouside AS */
+	u_int16_t ig_sum;	/* checksum of IGRP header & data */
+};
+
+#define IGRP_UPDATE	1
+#define IGRP_REQUEST	2
+
+/* IGRP routing entry */
+
+struct igrprte {
+	u_int8_t igr_net[3];	/* 3 significant octets of IP address */
+	u_int8_t igr_dly[3];	/* delay in tens of microseconds */
+	u_int8_t igr_bw[3];	/* bandwidth in units of 1 kb/s */
+	u_int8_t igr_mtu[2];	/* MTU in octets */
+	u_int8_t igr_rel;	/* percent packets successfully tx/rx */
+	u_int8_t igr_ld;	/* percent of channel occupied */
+	u_int8_t igr_hct;	/* hop count */
+};
+
+#define IGRP_RTE_SIZE	14	/* don't believe sizeof ! */
 
 static void
 igrp_entry_print(register struct igrprte *igr, register int is_interior,
