be63f9cc0b6de442107f1683dbb596a2177340cd
Guy Harris <guy@alum.mit.edu>
2014-01-16 17:25:05 -0800

    Use unaligned_memcmp() to compare with IPv{4,6} addresses in a packet.
    They are, after all, not guaranteed to be aligned.

--- print-esp.c
+++ print-esp.c
@@ -613,7 +613,7 @@ esp_print(netdissect_options *ndo,
 			struct sockaddr_in6 *sin6 = (struct sockaddr_in6 *)&sa->daddr;
 			if (sa->spi == EXTRACT_32BITS(&esp->esp_spi) &&
 			    sin6->sin6_family == AF_INET6 &&
-			    memcmp(&sin6->sin6_addr, &ip6->ip6_dst,
+			    unaligned_memcmp(&sin6->sin6_addr, &ip6->ip6_dst,
 				   sizeof(struct in6_addr)) == 0) {
 				break;
 			}
@@ -631,7 +631,8 @@ esp_print(netdissect_options *ndo,
 			struct sockaddr_in *sin = (struct sockaddr_in *)&sa->daddr;
 			if (sa->spi == EXTRACT_32BITS(&esp->esp_spi) &&
 			    sin->sin_family == AF_INET &&
-			    sin->sin_addr.s_addr == ip->ip_dst.s_addr) {
+			    unaligned_memcmp(&sin->sin_addr, &ip->ip_dst,
+				   sizeof(struct in_addr)) == 0) {
 				break;
 			}
 		}
