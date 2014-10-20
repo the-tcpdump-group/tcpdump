9401a3960ed5068bcd084632880dc9ee4a203327
Guy Harris <guy@alum.mit.edu>
2014-10-20 11:34:24 -0700

    Don't subtract the UDP header size from the length twice.

--- print-udp.c
+++ print-udp.c
@@ -690,8 +690,7 @@ udp_print(register const u_char *bp, u_int length,
 				printf("UDP, bad length %u > %u",
 				    ulen, length);
 			else
-				printf("UDP, length %u",
-				    (uint32_t)(ulen - sizeof(*up)));
+				printf("UDP, length %u", ulen);
 		}
 #undef ISPORT
 	} else {
@@ -699,8 +698,7 @@ udp_print(register const u_char *bp, u_int length,
 			printf("UDP, bad length %u > %u",
 			    ulen, length);
 		else
-			printf("UDP, length %u",
-			    (uint32_t)(ulen - sizeof(*up)));
+			printf("UDP, length %u", ulen);
 	}
 }
 
