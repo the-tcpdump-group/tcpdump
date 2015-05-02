5d284f271444d311032ba7cc458ceb7d91d90a76
Guy Harris <guy@alum.mit.edu>
2015-05-02 10:15:42 -0700

    Fix checksumming of PIMv2 Register messages.
    The checksum only covers the header, not the encapsulated packet, so
    only checksum that.  However, if that checksum fails, try checksumming
    the entire packet, as, according to RFC 4601, packets with the entire
    packet checksummed should also be accepted, for interoperability.

--- print-ip6.c
+++ print-ip6.c
@@ -223,8 +223,7 @@ ip6_print(netdissect_options *ndo, const u_char *bp, u_int length)
 		    }
 
 		case IPPROTO_PIM:
-			pim_print(ndo, cp, len, nextproto6_cksum(ip6, cp, len, len,
-							    IPPROTO_PIM));
+			pim_print(ndo, cp, len, (const u_char *)ip6);
 			return;
 
 		case IPPROTO_OSPF:
