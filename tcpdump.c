0b76ddba686f4d6cc4044b44d208b3386d853ab4
Brooks Davis <brooks@one-eyed-alien.net>
2015-05-05 17:50:52 +0000

    Remove "capability mode sandbox enabled" message.
    We have removed all instances of these messages in FreeBSD as they serve
    little purpose and break some comsumers.

--- tcpdump.c
+++ tcpdump.c
@@ -1686,8 +1686,6 @@ main(int argc, char **argv)
 	cansandbox = (gndo->ndo_nflag && VFileName == NULL && zflag == NULL);
 	if (cansandbox && cap_enter() < 0 && errno != ENOSYS)
 		error("unable to enter the capability mode");
-	if (cap_sandboxed())
-		fprintf(stderr, "capability mode sandbox enabled\n");
 #endif	/* HAVE_CAPSICUM */
 
 	do {
