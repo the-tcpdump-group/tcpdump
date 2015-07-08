64f6392084ec0768b8afc04612eac0a458bc5e0d
Guy Harris <guy@alum.mit.edu>
2015-07-07 20:08:55 -0700

    CVE-2016-7923/Don't assume the target hardware address is <= 6 octets long.
    It might not be, either because an unusual hardware type is using ARP or
    because a maliciously-constructed packet was sent.  Instead of comparing
    against a 6-octet string of zeros with memcmp(), check each octet of the
    address against 0.
    
    Fixes a heap overflow found with American Fuzzy Lop by Hanno Böck.

--- print-arp.c
+++ print-arp.c
@@ -177,7 +177,17 @@ struct  atmarp_pkthdr {
 #define ATMTSA(ap) (aar_tsa(ap))
 #define ATMTPA(ap) (aar_tpa(ap))
 
-static u_char ezero[6];
+static int
+isnonzero(const u_char *a, size_t len)
+{
+	while (len > 0) {
+		if (*a != 0)
+			return (1);
+		a++;
+		len--;
+	}
+	return (0);
+}
 
 static void
 atmarp_addr_print(netdissect_options *ndo,
@@ -358,7 +368,7 @@ arp_print(netdissect_options *ndo,
 
 	case ARPOP_REQUEST:
 		ND_PRINT((ndo, "who-has %s", ipaddr_string(ndo, TPA(ap))));
-		if (memcmp((const char *)ezero, (const char *)THA(ap), HRD_LEN(ap)) != 0)
+		if (isnonzero((const u_char *)THA(ap), HRD_LEN(ap)))
 			ND_PRINT((ndo, " (%s)",
 				  linkaddr_string(ndo, THA(ap), linkaddr, HRD_LEN(ap))));
 		ND_PRINT((ndo, " tell %s", ipaddr_string(ndo, SPA(ap))));
