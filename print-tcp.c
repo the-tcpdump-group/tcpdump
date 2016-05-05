3b9b4fa2c1be05cbcec47d50f3488b51440c1b36
Patrik Lundquist <patrik.lundquist@gmail.com>
2016-05-05 21:31:01 +0200

    Correct TCP option Kind value for TCP Auth and add SCPS-TP.
    Fixes first problem in issue #516 while the second one isn't broken in tcpdump.

--- print-tcp.c
+++ print-tcp.c
@@ -125,8 +125,9 @@ static const struct tok tcp_option_values[] = {
         { TCPOPT_CCNEW, "ccnew" },
         { TCPOPT_CCECHO, "" },
         { TCPOPT_SIGNATURE, "md5" },
-        { TCPOPT_AUTH, "enhanced auth" },
+        { TCPOPT_SCPS, "scps" },
         { TCPOPT_UTO, "uto" },
+        { TCPOPT_AUTH, "enhanced auth" },
         { TCPOPT_MPTCP, "mptcp" },
         { TCPOPT_FASTOPEN, "tfo" },
         { TCPOPT_EXPERIMENT2, "exp" },
@@ -532,6 +533,12 @@ tcp_print(netdissect_options *ndo,
                                 for (i = 0; i < TCP_SIGLEN; ++i)
                                         ND_PRINT((ndo, "%02x", cp[i]));
 #endif
+                                break;
+
+                        case TCPOPT_SCPS:
+                                datalen = 2;
+                                LENCHECK(datalen);
+                                ND_PRINT((ndo, " cap %02x id %u", cp[0], cp[1]));
                                 break;
 
                         case TCPOPT_AUTH:
