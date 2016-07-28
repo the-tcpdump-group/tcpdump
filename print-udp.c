75b7aeef9cc990ab705759884a725c1cad9924d0
Jean-Raphaël GAGLIONE <jr.gaglione@yahoo.fr>
2016-07-28 21:09:41 +0200

    Printer for HNCP (RFCs 7787 and 7788).
    This is joint work with Antonin Décimo.

--- print-udp.c
+++ print-udp.c
@@ -594,6 +594,8 @@ udp_print(netdissect_options *ndo, register const u_char *bp, u_int length,
 			ahcp_print(ndo, (const u_char *)(up + 1), length);
 		else if (IS_SRC_OR_DST_PORT(BABEL_PORT) || IS_SRC_OR_DST_PORT(BABEL_PORT_OLD))
 			babel_print(ndo, (const u_char *)(up + 1), length);
+		else if (IS_SRC_OR_DST_PORT(HNCP_PORT))
+			hncp_print(ndo, (const u_char *)(up + 1), length);
 		/*
 		 * Kludge in test for whiteboard packets.
 		 */
@@ -677,4 +679,3 @@ udp_print(netdissect_options *ndo, register const u_char *bp, u_int length,
  * c-basic-offset: 8
  * End:
  */
-
