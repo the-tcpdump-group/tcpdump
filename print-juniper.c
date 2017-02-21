a78485783d96aa0e49a737e8b0f45ab0a859c074
Guy Harris <guy@alum.mit.edu>
2017-02-21 14:20:32 -0800

    CVE-2017-13004/Juniper: Add a bounds check.
    This fixes a buffer over-read discovered by Forcepoint's security
    researchers Otto Airamo & Antti Levomäki.
    
    Add tests using the capture files supplied by the reporter(s).

--- print-juniper.c
+++ print-juniper.c
@@ -1367,6 +1367,7 @@ juniper_parse_header(netdissect_options *ndo,
             if (ndo->ndo_eflag) ND_PRINT((ndo, ": ")); /* print demarc b/w L2/L3*/
 
 
+            ND_TCHECK_16BITS(p+l2info->cookie_len);
             l2info->proto = EXTRACT_16BITS(p+l2info->cookie_len);
             break;
         }
