bb68c0c21bccd0685ba78ad5fdf479637a03b2b6
Job Snijders <job@instituut.net>
2017-05-30 09:52:02 +0000

    RFC 6608 specifies subcode 0 to be value "Unspecified Error"

--- print-bgp.c
+++ print-bgp.c
@@ -308,10 +308,10 @@ static const struct tok bgp_notify_minor_update_values[] = {
 };
 
 static const struct tok bgp_notify_minor_fsm_values[] = {
+    { 0,                        "Unspecified Error"},
     { 1,                        "In OpenSent State"},
     { 2,                        "In OpenConfirm State"},
-    { 3,                        "In Established State"},
-    { 0, NULL }
+    { 3,                        "In Established State"}
 };
 
 static const struct tok bgp_notify_minor_cap_values[] = {
