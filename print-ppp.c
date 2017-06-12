3f069d0e2ef3e39b7ae11503fb6286635ee07353
Guy Harris <guy@alum.mit.edu>
2017-06-11 18:27:27 -0700

    CVE-2017-13038/PPP: Do bounds checking.
    This fixes a buffer over-read discovered by Brian 'geeknik' Carpenter.
    
    Add a test using the capture file supplied by Katie Holly.

--- print-ppp.c
+++ print-ppp.c
@@ -811,6 +811,15 @@ handle_mlppp(netdissect_options *ndo,
     if (!ndo->ndo_eflag)
         ND_PRINT((ndo, "MLPPP, "));
 
+    if (length < 2) {
+        ND_PRINT((ndo, "[|mlppp]"));
+        return;
+    }
+    if (!ND_TTEST_16BITS(p)) {
+        ND_PRINT((ndo, "[|mlppp]"));
+        return;
+    }
+
     ND_PRINT((ndo, "seq 0x%03x, Flags [%s], length %u",
            (EXTRACT_16BITS(p))&0x0fff, /* only support 12-Bit sequence space for now */
            bittok2str(ppp_ml_flag_values, "none", *p & 0xc0),
