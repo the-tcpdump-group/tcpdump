d72adb9406a34998a96d148fa701cd4fb573e8b9
Denis Ovsienko <denis@ovsienko.info>
2017-07-13 21:07:13 +0100

    Fix a bug in test scripts (exposed in GH #613).
    crypto.sh ran its tests as expected but only indicated a failure
    upstream when the last test failed. Add necessary checks and modify
    the other "complex" tests in a similar manner to make sure the same
    bug does not creep into these scripts later. Fix some indentation
    while at it.

--- tests/lmp-v.sh
+++ tests/lmp-v.sh
@@ -6,6 +6,8 @@
 # GCC build and must reproduce correctly on any other GCC build regardless of
 # the architecture.
 
+exitcode=0
+
 # A Windows build may have no file named Makefile and also a version of grep
 # that won't return an error when the file does not exist. Work around.
 if [ ! -f ../Makefile ]
@@ -13,7 +15,10 @@ then
 	printf '    %-35s: TEST SKIPPED (no Makefile)\n' 'lmp-v'
 elif grep '^CC = .*gcc' ../Makefile >/dev/null
 then
-  ./TESTonce lmp-v lmp.pcap lmp-v.out '-T lmp -v'
+	./TESTonce lmp-v lmp.pcap lmp-v.out '-T lmp -v'
+	[ $? -eq 0 ] || exitcode=1
 else
 	printf '    %-35s: TEST SKIPPED (compiler is not GCC)\n' 'lmp-v'
 fi
+
+exit $exitcode
