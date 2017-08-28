53bb4d30e87830715751e14a2328d63ef93667d7
Ulrich Windl <Ulrich.Windl@RZ.Uni-Regensburg.DE>
2017-08-28 14:45:17 +0200

    Consistent status output in ntp_control_print()
    print-ntp.c: Change ntp_control_print() to print status in hex always.
    Update affected test output files.

--- print-ntp.c
+++ print-ntp.c
@@ -870,7 +870,7 @@ ntp_control_print(netdissect_options *ndo,
 
 	ND_TCHECK(cd->status);
 	status = EXTRACT_BE_U_2(&cd->status);
-	ND_PRINT((ndo, "%sStatus=%hu", indent, status));
+	ND_PRINT((ndo, "%sStatus=%#04x", indent, status));
 
 	ND_TCHECK(cd->assoc);
 	assoc = EXTRACT_BE_U_2(&cd->assoc);
@@ -900,7 +900,7 @@ ntp_control_print(netdissect_options *ndo,
 		/* See "3.3.  Clock Status Word" */
 		ntp_control_print_CSW(ndo, status, "\n\t\t");
 	} else {
-		ND_PRINT((ndo, ", Status=%#hx", status));
+		ND_PRINT((ndo, ", Status=%#04hx", status));
 	}
 
 	if ((int) (length - sizeof(*cd)) > 0)
