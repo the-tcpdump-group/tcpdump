18568bc04425a1a51e234fcb97c308dfb6a2a251
Ali Abdulkadir <autostart.ini@gmail.com>
2017-10-24 08:11:33 +0300

    Made up for missing getservent() and endservent()

--- addrtoname.c
+++ addrtoname.c
@@ -57,6 +57,9 @@ extern int ether_ntohost(char *, const struct ether_addr *);
 
 #include <pcap.h>
 #include <pcap-namedb.h>
+#ifndef HAVE_GETSERVENT
+#include <getservent.h>
+#endif
 #include <signal.h>
 #include <stdio.h>
 #include <string.h>
