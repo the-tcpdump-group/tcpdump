3582e7ac3d179510287a02861bed2b92f519d5d6
Guy Harris <guy@alum.mit.edu>
2018-02-01 14:19:08 -0800

    Don't decrement an unsigned value past zero.
    That also means we do one less decrement, so it's not as if testing
    exp-- rather than testing exp and separately decrementing it is an
    optimization.

--- print-olsr.c
+++ print-olsr.c
@@ -213,8 +213,9 @@ static uint32_t deserialize_gw_speed(uint8_t value) {
   speed = (value >> 3) + 1;
   exp = value & 7;
 
-  while (exp-- > 0) {
+  while (exp != 0) {
     speed *= 10;
+    exp--;
   }
   return speed;
 }
