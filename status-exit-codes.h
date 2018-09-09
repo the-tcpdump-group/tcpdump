d5ca317dbdd246205f88b62fea2e765341bc9706
Francois-Xavier Le Bail <devel.fx.lebail@orange.fr>
2018-09-09 14:27:22 +0200

    More status exit codes for a program using libnetdissect
    It can now use status exit codes 0 to 10.

--- status-exit-codes.h
+++ status-exit-codes.h
@@ -22,11 +22,11 @@
 typedef enum {
 	S_SUCCESS           = 0, /* not a libnetdissect status */
 	S_ERR_HOST_PROGRAM  = 1, /* not a libnetdissect status */
-	S_ERR_ND_NO_PRINTER = 2,
-	S_ERR_ND_MEM_ALLOC  = 3,
-	S_ERR_ND_OPEN_FILE  = 4,
-	S_ERR_ND_WRITE_FILE = 5,
-	S_ERR_ND_ESP_SECRET = 6
+	S_ERR_ND_NO_PRINTER = 11,
+	S_ERR_ND_MEM_ALLOC  = 12,
+	S_ERR_ND_OPEN_FILE  = 13,
+	S_ERR_ND_WRITE_FILE = 14,
+	S_ERR_ND_ESP_SECRET = 15
 } status_exit_codes_t;
 
 #endif /* status_exit_codes_h */
