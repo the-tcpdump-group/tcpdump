32af00b05a6ef573d0b340f97b54c13eb9509dc7
Pedro Monreal <pmgdeb@gmail.com>
2018-12-06 12:18:38 +0100

    CVE-2018-19519 buffer overread. Initialize buf in print-hncp.c:print_prefix.

--- print-hncp.c
+++ print-hncp.c
@@ -208,6 +208,8 @@ print_prefix(netdissect_options *ndo, const u_char *prefix, u_int max_length)
     int plenbytes;
     char buf[sizeof("xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx::/128")];
 
+    buf[0] = '\0';
+    
     if (EXTRACT_U_1(prefix) >= 96 && max_length >= IPV4_MAPPED_HEADING_LEN + 1 &&
         is_ipv4_mapped_address(prefix + 1)) {
         struct in_addr addr;
