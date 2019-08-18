afe2f6bf9acfa1e7d47beeab290fd6d293331e98
Francois-Xavier Le Bail <devel.fx.lebail@orange.fr>
2019-08-18 17:47:19 +0200

    LDP: Fix an undefined behavior at runtime
    The error was:
    print-ldp.c:557:13: runtime error: unsigned integer overflow: 32 - 34
    cannot be represented in type 'unsigned int'
    
    (backported from commit f50b5d79085dd3c5212b52a99697cb4ccd4dea6d)

--- print-ldp.c
+++ print-ldp.c
@@ -210,7 +210,7 @@ static const struct tok ldp_fec_martini_ifparm_vccv_cv_values[] = {
     { 0, NULL}
 };
 
-static int ldp_pdu_print(netdissect_options *, register const u_char *);
+static u_int ldp_pdu_print(netdissect_options *, register const u_char *);
 
 /*
  * ldp tlv header
@@ -546,17 +546,23 @@ void
 ldp_print(netdissect_options *ndo,
           register const u_char *pptr, register u_int len)
 {
-    int processed;
+    u_int processed;
     while (len > (sizeof(struct ldp_common_header) + sizeof(struct ldp_msg_header))) {
         processed = ldp_pdu_print(ndo, pptr);
         if (processed == 0)
             return;
+        if (len < processed) {
+            ND_PRINT((ndo, " [remaining length %u < %u]", len, processed));
+            ND_PRINT((ndo, "%s", istr));
+            break;
+
+        }
         len -= processed;
         pptr += processed;
     }
 }
 
-static int
+static u_int
 ldp_pdu_print(netdissect_options *ndo,
               register const u_char *pptr)
 {
