8dbfd7f299b06e19c4ede893bf35f366386f5cad
Francois-Xavier Le Bail <devel.fx.lebail@orange.fr>
2019-09-02 12:08:23 +0200

    Use quoted include netdissect-stdinc.h instead of angle-bracketed one

--- print-arista.c
+++ print-arista.c
@@ -6,7 +6,7 @@
 #include "config.h"
 #endif
 
-#include <netdissect-stdinc.h>
+#include "netdissect-stdinc.h"
 
 #include <string.h>
 
--- print-ipoib.c
+++ print-ipoib.c
@@ -29,7 +29,7 @@
 #include <config.h>
 #endif
 
-#include <netdissect-stdinc.h>
+#include "netdissect-stdinc.h"
 
 #include <stdio.h>
 
