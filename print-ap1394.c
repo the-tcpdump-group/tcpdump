2ca64998d7d45d94806cd972358ca4495d0c857d
Denis Ovsienko <denis@ovsienko.info>
2020-10-10 22:39:18 +0100

    Apple IPoIEEE1394: Switch to nd_trunc_longjmp(). [skip ci]

--- print-ap1394.c
+++ print-ap1394.c
@@ -97,8 +97,7 @@ ap1394_if_print(netdissect_options *ndo, const struct pcap_pkthdr *h, const u_ch
 	ndo->ndo_protocol = "ap1394";
 	if (caplen < FIREWIRE_HDRLEN) {
 		ndo->ndo_ll_hdr_len += caplen;
-		nd_print_trunc(ndo);
-		return;
+		nd_trunc_longjmp(ndo);
 	}
 	ndo->ndo_ll_hdr_len += FIREWIRE_HDRLEN;
 
