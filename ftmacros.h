8f5c182a14f4f0299718bbfffb31a5d961670701
Guy Harris <gharris@sonic.net>
2021-01-02 01:06:36 -0800

    Fix a build problem on the Linux Travis CI buildbot.
    The buildbot is failing in autotools builds - but not in CMake builds;
    go figure.  Perhaps they're passing different flags to the C compiler,
    and, in the autotools builds, the flag causes the compiler to define
    whatever it takes to get /usr/include/features.h to define
    _DEFAULT_SOURCE as 1, so that our defining it as nothing collides with
    that.
    
    (Dear UN*X community: please come up with a platform-independent,
    reliable way of saying "expose every single API you have, namespace
    pollution be damned.  kthxbye.)
    
    (cherry picked from commit a689cf870986a04cf7a8b4fc74cab10e808f3c93)

--- ftmacros.h
+++ ftmacros.h
@@ -105,8 +105,14 @@
    * don't whine about _BSD_SOURCE being deprecated; we still have
    * to define _BSD_SOURCE to handle older versions of GNU libc that
    * don't support _DEFAULT_SOURCE.
+   *
+   * But, if it's already defined, don't define it, so that we don't
+   * get a warning of it being redefined if it's defined as, for
+   * example, 1.
    */
-  #define _DEFAULT_SOURCE
+  #ifndef _DEFAULT_SOURCE
+    #define _DEFAULT_SOURCE
+  #endif
   #define _BSD_SOURCE
 #endif
 
