502bb6a602bf49a74f673b0c3aaf13f4d83da5e1
Guy Harris <gharris@sonic.net>
2021-08-11 01:28:42 -0700

    Fix a comment.  [skip ci]
    (cherry picked from commit b3626b4c729ddb81b43ef58bee5d324b94f8908a)

--- funcattrs.h
+++ funcattrs.h
@@ -128,7 +128,8 @@
     || ND_IS_AT_LEAST_XL_C_VERSION(10,1) \
     || ND_IS_AT_LEAST_HP_C_VERSION(6,10)
   /*
-   * Compiler with support for it, or GCC 2.3 and later, or IBM XL C 10.1
+   * Compiler with support for it, or GCC 2.3 and later, or some compiler
+   * asserting compatibility with GCC 2.3 and later, or IBM XL C 10.1
    * and later (do any earlier versions of XL C support this?),
    * or HP aCC A.06.10 and later.
    */
