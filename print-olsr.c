2e99bc485ab304df2d4c797af9576ad1b167c0f8
Francois-Xavier Le Bail <devel.fx.lebail@orange.fr>
2023-04-21 14:24:23 +0200

    Put "{" at end of line with "else" to keep a consistent style
    [skip ci]

--- print-olsr.c
+++ print-olsr.c
@@ -381,8 +381,7 @@ olsr_print(netdissect_options *ndo,
             msg_tlen = msg_len - sizeof(struct olsr_msg6);
             msg_data = tptr + sizeof(struct olsr_msg6);
         }
-        else /* (!is_ipv6) */
-        {
+        else {	/* (!is_ipv6) */
             ND_TCHECK_LEN(tptr, sizeof(struct olsr_msg4));
             msgptr.v4 = (const struct olsr_msg4 *) tptr;
             msg_type = GET_U_1(msgptr.v4->msg_type);
@@ -537,8 +536,7 @@ olsr_print(netdissect_options *ndo,
                     msg_tlen -= sizeof(struct olsr_hna6);
                 }
             }
-            else
-            {
+            else {
                 int col = 0;
 
                 ND_PRINT("\n\t  Advertised networks (total %u)",
