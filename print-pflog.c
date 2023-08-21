a0b785957c77e014b566949bf00ff746e2071813
Guy Harris <gharris@sonic.net>
2023-08-21 13:37:44 -0700

    pflog: use nd_ types in struct pfloghdr.
    This 1) makes sure that GET_ macros are used to extract data from the
    structure (which they already were) and 2) avoids undefined behavior if
    the structure isn't aligned on the appropriate memory boundary.
    
    Fixes #1054.  (The SNMP issues are fixed by changes for #1012.)

--- print-pflog.c
+++ print-pflog.c
@@ -106,8 +106,8 @@ pflog_print(netdissect_options *ndo, const struct pfloghdr *hdr)
 	uint32_t rulenr, subrulenr;
 
 	ndo->ndo_protocol = "pflog";
-	rulenr = GET_BE_U_4(&hdr->rulenr);
-	subrulenr = GET_BE_U_4(&hdr->subrulenr);
+	rulenr = GET_BE_U_4(hdr->rulenr);
+	subrulenr = GET_BE_U_4(hdr->subrulenr);
 	if (subrulenr == (uint32_t)-1)
 		ND_PRINT("rule %u/", rulenr);
 	else {
@@ -117,9 +117,9 @@ pflog_print(netdissect_options *ndo, const struct pfloghdr *hdr)
 	}
 
 	ND_PRINT("%s: %s %s on ",
-	    tok2str(pf_reasons, "unkn(%u)", GET_U_1(&hdr->reason)),
-	    tok2str(pf_actions, "unkn(%u)", GET_U_1(&hdr->action)),
-	    tok2str(pf_directions, "unkn(%u)", GET_U_1(&hdr->dir)));
+	    tok2str(pf_reasons, "unkn(%u)", GET_U_1(hdr->reason)),
+	    tok2str(pf_actions, "unkn(%u)", GET_U_1(hdr->action)),
+	    tok2str(pf_directions, "unkn(%u)", GET_U_1(hdr->dir)));
 	nd_printjnp(ndo, (const u_char*)hdr->ifname, PFLOG_IFNAMSIZ);
 	ND_PRINT(": ");
 }
@@ -144,12 +144,13 @@ pflog_if_print(netdissect_options *ndo, const struct pcap_pkthdr *h,
 
 #define MIN_PFLOG_HDRLEN	45
 	hdr = (const struct pfloghdr *)p;
-	if (GET_U_1(&hdr->length) < MIN_PFLOG_HDRLEN) {
+	hdrlen = GET_U_1(hdr->length);
+	if (hdrlen < MIN_PFLOG_HDRLEN) {
 		ND_PRINT("[pflog: invalid header length!]");
-		ndo->ndo_ll_hdr_len += GET_U_1(&hdr->length);	/* XXX: not really */
+		ndo->ndo_ll_hdr_len += hdrlen;	/* XXX: not really */
 		return;
 	}
-	hdrlen = roundup2(hdr->length, 4);
+	hdrlen = roundup2(hdrlen, 4);
 
 	if (caplen < hdrlen) {
 		nd_print_trunc(ndo);
@@ -163,7 +164,7 @@ pflog_if_print(netdissect_options *ndo, const struct pcap_pkthdr *h,
 		pflog_print(ndo, hdr);
 
 	/* skip to the real packet */
-	af = GET_U_1(&hdr->af);
+	af = GET_U_1(hdr->af);
 	length -= hdrlen;
 	caplen -= hdrlen;
 	p += hdrlen;
