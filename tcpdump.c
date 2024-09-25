aacc29d06ccccd15b6b07bba0858e6fa32e97edd
Francois-Xavier Le Bail <devel.fx.lebail@orange.fr>
2024-09-25 13:47:06 +0200

    Add --time-t-size option to print the size of time_t in bits
    Have tests/TESTrun run "./tcpdump --time-t-size and set HAVE_TIME_T_64
    key based on that.
    
    This will permit to run tests based on time_t size (32-bit or 64-bit).

--- tcpdump.c
+++ tcpdump.c
@@ -641,6 +641,7 @@ show_remote_devices_and_exit(void)
 #define OPTION_COUNT			136
 #define OPTION_PRINT_SAMPLING		137
 #define OPTION_LENGTHS			138
+#define OPTION_TIME_T_SIZE		139
 
 static const struct option longopts[] = {
 	{ "buffer-size", required_argument, NULL, 'B' },
@@ -682,6 +683,7 @@ static const struct option longopts[] = {
 	{ "print", no_argument, NULL, OPTION_PRINT },
 	{ "print-sampling", required_argument, NULL, OPTION_PRINT_SAMPLING },
 	{ "lengths", no_argument, NULL, OPTION_LENGTHS },
+	{ "time-t-size", no_argument, NULL, OPTION_TIME_T_SIZE },
 	{ "version", no_argument, NULL, OPTION_VERSION },
 	{ NULL, 0, NULL, 0 }
 };
@@ -1869,6 +1871,10 @@ main(int argc, char **argv)
 		case OPTION_LENGTHS:
 			ndo->ndo_lengths = 1;
 			break;
+
+		case OPTION_TIME_T_SIZE:
+			printf("%zu\n", sizeof(time_t) * 8);
+			return 0;
 
 		case OPTION_VERSION:
 			print_version(stdout);
