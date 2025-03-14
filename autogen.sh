8b6db826299d843889933bb3f76bf7ca8caefaa9
Francois-Xavier Le Bail <devel.fx.lebail@orange.fr>
2025-03-14 23:36:01 +0100

    Autoconf: Use AC_SYS_YEAR2038_RECOMMENDED when possible
    On Linux, if the environment variable BUILD_YEAR2038=yes, Autoconf
    version >= 2.72 and GNU C Library version >= 2.34, uncomment
    AC_SYS_YEAR2038_RECOMMENDED to ensure time_t is Y2038-safe.
    
    Tested with: BUILD_YEAR2038=yes MATRIX_CMAKE=no ./build_matrix.sh
    on linux-armv7l because currently CMake build don't have year 2038 support.
    
    (backported from commit 7cd08fe7c4a07df9028084612f81f90bd1b8a412)

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 
 : "${AUTORECONF:=autoreconf}"
+: "${BUILD_YEAR2038:=no}"
 
 AUTORECONFVERSION=`$AUTORECONF --version 2>&1 | grep "^autoreconf" | sed 's/.*) *//'`
 
@@ -19,6 +20,27 @@ if [ "$maj" = "" ] || [ "$min" = "" ] || \
 	AUTORECONF=autoreconf-2.71 ./autogen.sh
 	EOF
 	exit 1
+fi
+
+# On Linux, if Autoconf version >= 2.72 and GNU C Library version >= 2.34,
+# s/AC_SYS_LARGEFILE/AC_SYS_YEAR2038_RECOMMENDED/ to ensure time_t
+# is Y2038-safe.
+if [ "$BUILD_YEAR2038" = yes ] && [ "`uname -s`" = Linux ]; then
+	if [ "$maj" -gt 2 ] || { [ "$maj" -eq 2 ] && [ "$min" -ge 72 ]; }; then
+		GLIBC_VERSION=`ldd --version|head -1|grep GLIBC|sed 's/.* //'`
+		maj_glibc=`echo "$GLIBC_VERSION" | cut -d. -f1`
+		min_glibc=`echo "$GLIBC_VERSION" | cut -d. -f2`
+		if [ "$maj_glibc" -gt 2 ] || { [ "$maj_glibc" -eq 2 ] && \
+		   [ "$min_glibc" -ge 34 ]; }; then
+			CONFIGURE_AC_NEW="configure.ac.new$$"
+			sed 's/^# \(AC_SYS_YEAR2038_RECOMMENDED\)/\1/' \
+				<configure.ac >"$CONFIGURE_AC_NEW"
+			cmp -s configure.ac "$CONFIGURE_AC_NEW" || \
+			cat "$CONFIGURE_AC_NEW" >configure.ac
+			rm -f "$CONFIGURE_AC_NEW"
+			echo 'Setup to ensure time_t is Y2038-safe.'
+		fi
+	fi
 fi
 
 echo "$AUTORECONF identification: $AUTORECONFVERSION"
