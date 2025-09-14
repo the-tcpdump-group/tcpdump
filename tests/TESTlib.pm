181b64b1789e7c357b399ab0f057363600e65d61
Francois-Xavier Le Bail <devel.fx.lebail@orange.fr>
2025-09-14 20:58:33 +0200

    Avoid non-ASCII characters
    UTF-8 left single quotation mark -> straight quote (').
    UTF-8 right single quotation mark -> straight quote (').
    
    [skip ci]

--- tests/TESTlib.pm
+++ tests/TESTlib.pm
@@ -153,8 +153,8 @@ sub skip_config_undef {
 sub skip_config_have_decl {
 	my ($name, $value) = @_;
 	$name = 'HAVE_DECL_' . $name;
-	# "Unlike the other ‘AC_CHECK_*S’ macros, when a symbol is not declared,
-	# HAVE_DECL_symbol is defined to ‘0’ instead of leaving HAVE_DECL_symbol
+	# "Unlike the other 'AC_CHECK_*S' macros, when a symbol is not declared,
+	# HAVE_DECL_symbol is defined to '0' instead of leaving HAVE_DECL_symbol
 	# undeclared." -- GNU Autoconf manual.
 	#
 	# (This requires the CMake leg to do the same for the same symbol.)
