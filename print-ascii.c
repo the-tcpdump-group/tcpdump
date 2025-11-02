b5f77ca7f7bb76d9a81f55a583cf49009c75f081
Jinzhou Zhang <lotabout@gmail.com>
2025-11-02 18:31:04 +0800

    fix #1190 support to display packet content as UTF-8

--- print-ascii.c
+++ print-ascii.c
@@ -44,6 +44,13 @@
 
 #include <stdio.h>
 
+#include <wchar.h>
+#include <wctype.h>
+
+#ifndef HAVE_WCWIDTH
+#include "missing/wcwidth.h"
+#endif
+
 #include "netdissect-ctype.h"
 
 #include "netdissect.h"
@@ -55,6 +62,69 @@
 #define HEXDUMP_HEXSTUFF_PER_SHORT 5 /* 4 hex digits and a space */
 #define HEXDUMP_HEXSTUFF_PER_LINE \
 		(HEXDUMP_HEXSTUFF_PER_SHORT * HEXDUMP_SHORTS_PER_LINE)
+
+
+/*
+ * The blow is_utf8_printable is taken from ngrep
+ *
+ * Check if a UTF-8 character sequence is printable using standard library functions.
+ * Returns the number of bytes in the UTF-8 character if printable, 0 otherwise.
+ * Also returns the display width (1 or 2 columns) via the width_out parameter.
+ *
+ * This uses mbrtowc() to convert multi-byte UTF-8 to wide char, then iswprint()
+ * to check if it's printable, and wcwidth() to get the display width.
+ */
+static u_int is_utf8_printable(const unsigned char *s, size_t max_len, int *width_out) {
+	if (!s || max_len == 0) return 0;
+
+	mbstate_t state = {0};
+	wchar_t wc;
+
+	size_t len = mbrtowc(&wc, (const char *)s, max_len, &state);
+
+	/* Check for errors and incomplete sequences */
+	if (len == (size_t)-1) {
+		/* Encoding error */
+		return 0;
+	}
+
+	if (len == (size_t)-2) {
+		/* Incomplete multi-byte sequence (need more bytes) */
+		return 0;
+	}
+
+	if (len == 0) {
+		/* Null character */
+		return 0;
+	}
+
+	/* Check if the wide character is printable */
+#if defined(_WIN32) || defined(_WIN64)
+	/* Windows iswprint() is too conservative - be more permissive for UTF-8 */
+	/* Accept any valid UTF-8 character that's not a control character */
+	int is_printable = iswprint(wc) ||
+		(wc >= 0x80 && wc < 0xD800) ||  /* Most of BMP except surrogates */
+		(wc >= 0xE000 && wc < 0x110000); /* Private use + supplementary planes */
+
+	/* But exclude actual control characters */
+	if (wc < 0x20 || (wc >= 0x7F && wc < 0xA0)) {
+		is_printable = 0;
+	}
+#else
+	int is_printable = iswprint(wc);
+#endif
+
+	if (is_printable) {
+		/* Get display width (1 for normal chars, 2 for wide chars like CJK, 0 for combining) */
+		int w = wcwidth(wc);
+		if (w < 0) w = 1;  /* Treat non-printable/control as width 1 */
+		/* Note: wcwidth returns 0 for combining characters, which is correct */
+		if (width_out) *width_out = w;
+		return (u_int)len;
+	}
+
+	return 0;
+}
 
 void
 ascii_print(netdissect_options *ndo,
@@ -71,28 +141,43 @@ ascii_print(netdissect_options *ndo,
 		truncated = TRUE;
 	}
 	ND_PRINT("\n");
-	while (length != 0) {
-		s = GET_U_1(cp);
-		cp++;
-		length--;
-		if (s == '\r') {
-			/*
-			 * Don't print CRs at the end of the line; they
-			 * don't belong at the ends of lines on UN*X,
-			 * and the standard I/O library will give us one
-			 * on Windows so we don't need to print one
-			 * ourselves.
-			 *
-			 * In the middle of a line, just print a '.'.
-			 */
-			if (length > 1 && GET_U_1(cp) != '\n')
-				ND_PRINT(".");
+
+	while (length > 0) {
+		int utf8_len;
+		int j;
+
+		utf8_len = ndo->ndo_utf8 ? is_utf8_printable(cp, length, NULL) : 0;
+
+		if (utf8_len > 0) {
+			/* Valid printable UTF-8 character */
+			for (j = 0; j < utf8_len; j++)
+				ND_PRINT("%c", cp[j]);
+			cp += utf8_len;
+			length -= utf8_len;
+
 		} else {
-			if (!ND_ASCII_ISGRAPH(s) &&
-			    (s != '\t' && s != ' ' && s != '\n'))
-				ND_PRINT(".");
-			else
-				ND_PRINT("%c", s);
+			s = GET_U_1(cp);
+			cp++;
+			length--;
+			if (s == '\r') {
+				/*
+				 * Don't print CRs at the end of the line; they
+				 * don't belong at the ends of lines on UN*X,
+				 * and the standard I/O library will give us one
+				 * on Windows so we don't need to print one
+				 * ourselves.
+				 *
+				 * In the middle of a line, just print a '.'.
+				 */
+				if (length > 1 && GET_U_1(cp) != '\n')
+					ND_PRINT(".");
+			} else {
+				if (!ND_ASCII_ISGRAPH(s) &&
+						(s != '\t' && s != ' ' && s != '\n'))
+					ND_PRINT(".");
+				else
+					ND_PRINT("%c", s);
+			}
 		}
 	}
 	if (truncated)
@@ -104,52 +189,67 @@ hex_and_ascii_print_with_offset(netdissect_options *ndo, const char *indent,
 				const u_char *cp, u_int length, u_int offset)
 {
 	u_int caplength;
-	u_int i;
-	u_int s1, s2;
-	u_int nshorts;
+	u_int nbytes_unprinted;
+	u_int s1;
 	int truncated = FALSE;
 	char hexstuff[HEXDUMP_SHORTS_PER_LINE*HEXDUMP_HEXSTUFF_PER_SHORT+1], *hsp;
-	char asciistuff[ASCII_LINELENGTH+1], *asp;
+	char asciistuff[ASCII_LINELENGTH+1+4], *asp;
+	u_int utf8_bytes_to_skip = 0;
 
 	caplength = ND_BYTES_AVAILABLE_AFTER(cp);
 	if (length > caplength) {
 		length = caplength;
 		truncated = TRUE;
 	}
-	nshorts = length / sizeof(u_short);
-	i = 0;
+	nbytes_unprinted = 0;
 	hsp = hexstuff; asp = asciistuff;
-	while (nshorts != 0) {
+	while (length != 0) {
 		s1 = GET_U_1(cp);
+
+		// insert the leading space of short
+		if ((hsp - hexstuff) % HEXDUMP_HEXSTUFF_PER_SHORT == 0) {
+			(void)snprintf(hsp, sizeof(hexstuff) - (hsp - hexstuff), " ");
+			hsp++;
+		}
+
+		// add the byte
+		(void)snprintf(hsp, sizeof(hexstuff) - (hsp - hexstuff), "%02x", s1);
+		hsp += 2;
+
+		if (utf8_bytes_to_skip > 0) {
+			// only pad the new line
+			if (nbytes_unprinted == (u_int)(asp - asciistuff)) {
+				*(asp++) = ' ';
+			}
+			utf8_bytes_to_skip--;
+		} else {
+			// try to add the display (utf8) chars
+			utf8_bytes_to_skip = ndo->ndo_utf8 ? is_utf8_printable(cp, length, NULL) : 0;
+			if (utf8_bytes_to_skip > 0) {
+				u_int j;
+				for (j=0; j<utf8_bytes_to_skip; j++) {
+					*(asp++) = (char)GET_U_1(cp+j);
+				}
+				utf8_bytes_to_skip --;
+			} else {
+				*(asp++) = (char)(ND_ASCII_ISGRAPH(s1) ? s1 : '.');
+			}
+		}
+
 		cp++;
-		s2 = GET_U_1(cp);
-		cp++;
-		(void)snprintf(hsp, sizeof(hexstuff) - (hsp - hexstuff),
-		    " %02x%02x", s1, s2);
-		hsp += HEXDUMP_HEXSTUFF_PER_SHORT;
-		*(asp++) = (char)(ND_ASCII_ISGRAPH(s1) ? s1 : '.');
-		*(asp++) = (char)(ND_ASCII_ISGRAPH(s2) ? s2 : '.');
-		i++;
-		if (i >= HEXDUMP_SHORTS_PER_LINE) {
+		nbytes_unprinted++;
+		if (nbytes_unprinted >= (HEXDUMP_SHORTS_PER_LINE * sizeof(u_short))) {
 			*hsp = *asp = '\0';
 			ND_PRINT("%s0x%04x: %-*s  %s",
 			    indent, offset, HEXDUMP_HEXSTUFF_PER_LINE,
 			    hexstuff, asciistuff);
-			i = 0; hsp = hexstuff; asp = asciistuff;
+			nbytes_unprinted = 0; hsp = hexstuff; asp = asciistuff;
 			offset += HEXDUMP_BYTES_PER_LINE;
 		}
-		nshorts--;
+		length--;
 	}
-	if (length & 1) {
-		s1 = GET_U_1(cp);
-		cp++;
-		(void)snprintf(hsp, sizeof(hexstuff) - (hsp - hexstuff),
-		    " %02x", s1);
-		hsp += 3;
-		*(asp++) = (char)(ND_ASCII_ISGRAPH(s1) ? s1 : '.');
-		++i;
-	}
-	if (i > 0) {
+
+	if (nbytes_unprinted > 0) {
 		*hsp = *asp = '\0';
 		ND_PRINT("%s0x%04x: %-*s  %s",
 		     indent, offset, HEXDUMP_HEXSTUFF_PER_LINE,
@@ -158,6 +258,7 @@ hex_and_ascii_print_with_offset(netdissect_options *ndo, const char *indent,
 	if (truncated)
 		nd_trunc_longjmp(ndo);
 }
+
 
 void
 hex_and_ascii_print(netdissect_options *ndo, const char *indent,
